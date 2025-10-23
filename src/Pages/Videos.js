c8f58e0eda870beecf8f380ac72a24b1d156ecfa
Oscar Dejesus <oscar6dejesus@gmail.com>
2025-10-23 12:57:18 -0400

    Remove GitHub Actions workflows

---
+++ src/Pages/Videos.js
@@ -0,0 +1,27 @@
+
+import UI from '../UI.js';
+import Video from '../Video.js';
+export default function Videos(){
+
+    return(
+        <>
+        <Video/>
+        <div className="Info">
+            <h1 className="Info-text">These are  Videos Ive uploaded to Youtube I wanted to upload these songs online and created the background and edited them myself.</h1>
+
+            
+        </div>
+        <div className="Info">
+            <h1>
+                <a href="https://youtu.be/KeJBAL30oas?si=AHPISn-kayEl1NM5">Echos</a>
+            </h1>
+            <h1> &nbsp;&nbsp;&nbsp;</h1>
+            <h1>
+                <a href="https://youtu.be/MIECpZ10PNs?si=y0TMj6i3VgWLhWEP">Woods</a>
+            </h1>
+        </div>
+        <UI/>
+
+        </>
+    );
+}
